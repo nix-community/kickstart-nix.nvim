e2a502c4583f4c9e3266c19fae2044e4ff6fd43f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-21 02:59:59 +0200

    feat: disable gitsigns.current_line_blame by default

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -1,6 +1,6 @@
 vim.schedule(function()
   require('gitsigns').setup {
-    current_line_blame = true,
+    current_line_blame = false,
     current_line_blame_opts = {
       ignore_whitespace = true,
     },
