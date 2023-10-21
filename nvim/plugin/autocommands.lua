4cb606fa69468c0f3ea9123aab2c1790131e25f4
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-21 03:03:32 +0200

    feat: comment out numbertoggle autocommand

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -10,30 +10,6 @@ api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
--- Toggle between relative/absolute line numbers
--- Show relative line numbers in the current buffer,
--- absolute line numbers in inactive buffers
-local numbertoggle = api.nvim_create_augroup('numbertoggle', { clear = true })
-api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
-  pattern = '*',
-  group = numbertoggle,
-  callback = function()
-    if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
-      vim.opt.relativenumber = true
-    end
-  end,
-})
-api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
-  pattern = '*',
-  group = numbertoggle,
-  callback = function()
-    if vim.o.nu then
-      vim.opt.relativenumber = false
-      vim.cmd.redraw()
-    end
-  end,
-})
-
 -- Disable spell checking in terminal buffers
 local nospell_group = api.nvim_create_augroup('nospell', { clear = true })
 api.nvim_create_autocmd('TermOpen', {
@@ -142,3 +118,29 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end,
 })
+
+-- More examples, disabled by default
+
+-- Toggle between relative/absolute line numbers
+-- Show relative line numbers in the current buffer,
+-- absolute line numbers in inactive buffers
+-- local numbertoggle = api.nvim_create_augroup('numbertoggle', { clear = true })
+-- api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+--   pattern = '*',
+--   group = numbertoggle,
+--   callback = function()
+--     if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
+--       vim.opt.relativenumber = true
+--     end
+--   end,
+-- })
+-- api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
+--   pattern = '*',
+--   group = numbertoggle,
+--   callback = function()
+--     if vim.o.nu then
+--       vim.opt.relativenumber = false
+--       vim.cmd.redraw()
+--     end
+--   end,
+-- })
