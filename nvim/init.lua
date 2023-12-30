eb0214e5638654befc2e053278b1d2bc2d5c5593
Marc Jakobi <marc@jakobi.dev>
2023-12-30 20:40:37 +0100

    fix: workaround for neovim syntax/ftplugin bug

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,6 @@ local g = vim.g
 -- g.mapleader = ' '
 -- g.maplocalleader = ' '
 
-cmd.syntax('on')
-cmd.syntax('enable')
 opt.compatible = false
 
 -- Enable true colour support
@@ -109,3 +107,9 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
+
+-- this should be at the end, because
+-- it causes neovim to source ftplugins
+-- on the packpath when passing a file to the nvim command
+cmd.syntax('on')
+cmd.syntax('enable')
