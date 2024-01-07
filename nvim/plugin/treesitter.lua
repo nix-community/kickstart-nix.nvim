806cb3d01823f412a29cffdce4bce2980b809dc8
Marc Jakobi <marc.jakobi@tiko.energy>
2024-01-07 20:08:33 +0100

    fix: update nvim_ts_context_comment_string config

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,4 +1,5 @@
 local configs = require('nvim-treesitter.configs')
+vim.g.skip_ts_context_comment_string_module = true
 configs.setup {
   -- ensure_installed = 'all',
   -- auto_install = false, -- Do not automatically install missing parsers when entering buffer
@@ -76,14 +77,13 @@ configs.setup {
       },
     },
   },
-  context_commentstring = {
-    enable = true,
-  },
 }
 
 require('treesitter-context').setup {
   max_lines = 3,
 }
+
+require('ts_context_commentstring').setup()
 
 -- Tree-sitter based folding
 -- vim.opt.foldmethod = 'expr'
