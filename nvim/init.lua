18d5fa7976562bbf799fe3e57b984c406a64d2f1
Marc Jakobi <marc@jakobi.dev>
2024-05-12 15:30:34 +0200

    fix: remove obsolete `syntax` command invocations (#22)

--- nvim/init.lua
+++ nvim/init.lua
@@ -107,9 +107,3 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
-
--- this should be at the end, because
--- it causes neovim to source ftplugins
--- on the packpath when passing a file to the nvim command
-cmd.syntax('on')
-cmd.syntax('enable')
