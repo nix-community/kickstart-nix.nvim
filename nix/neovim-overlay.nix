b7eb9fe9c0121a2513adb3c84c1eacca152607b8
Slava <visla.vvi@gmail.com>
2025-04-04 19:24:40 +0500

    Some changes here and there (#62)
    * feat(nvim): move `vim.loader.enable` to init.lua
    
    This way it doesn't depend on the value of `wrapRc`
    
    * refactor(nvim): remove disabling `compatible`
    
    it's always disabled in neovim
    
    :h nvim-defaults
    ```
    'compatible' is always disabled
    ```
    
    * refactor(nvim): don't set termguicolors, since neovim enables it by default
    
    * refactor(nvim): editorconfig is enabled by default. No need to enable it explicitly
    
    * feat(nvim): fixup typo in treesitter.lua `nsp_interop->lsp_interop`
    
    * refactor(overlay,mkNeovim): hoist dependencies on nixpkgs
    
    Let them belong to the setup of `mkNeovim` function.
    
    * refactor(init.lua): move 'colorcolumn' to the rest of the options
    
    * doc(nvim/init.lua): comment on mapleader
    
    Co-authored-by: Marc Jakobi <mrcjkb89@outlook.com>
    
    ---------
    
    Co-authored-by: Slava <slava.istomin@tuta.io>
    Co-authored-by: Marc Jakobi <mrcjkb89@outlook.com>

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -12,10 +12,12 @@ with final.pkgs.lib; let
 
   # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
   # otherwise it could have an incompatible signature when applying this overlay.
-  pkgs-wrapNeovim = inputs.nixpkgs.legacyPackages.${pkgs.system};
+  pkgs-locked = inputs.nixpkgs.legacyPackages.${pkgs.system};
 
   # This is the helper function that builds the Neovim derivation.
-  mkNeovim = pkgs.callPackage ./mkNeovim.nix { inherit pkgs-wrapNeovim; };
+  mkNeovim = pkgs.callPackage ./mkNeovim.nix {
+      inherit (pkgs-locked) wrapNeovimUnstable neovimUtils;
+    };
 
   # A plugin can either be a package or an attrset, such as
   # { plugin = <plugin>; # the package, e.g. pkgs.vimPlugins.nvim-cmp
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,12 @@
+vim.loader.enable()
+
 local cmd = vim.cmd
-local fn = vim.fn
 local opt = vim.o
-local g = vim.g
 
 -- <leader> key. Defaults to `\`. Some people prefer space.
--- g.mapleader = ' '
--- g.maplocalleader = ' '
-
-opt.compatible = false
-
--- Enable true colour support
-if fn.has('termguicolors') then
-  opt.termguicolors = true
-end
+-- The default leader is '\'. Some people prefer <space>. Uncomment this if you do, too.
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
 
 -- See :h <option> to see what the options do
 
@@ -43,6 +37,7 @@ opt.splitbelow = true
 opt.cmdheight = 0
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+opt.colorcolumn = '100'
 
 -- Configure Neovim diagnostic messages
 
@@ -91,10 +86,6 @@ vim.diagnostic.config {
     prefix = '',
   },
 }
-
-g.editorconfig = true
-
-vim.opt.colorcolumn = '100'
 
 -- Native plugins
 cmd.filetype('plugin', 'indent', 'on')
